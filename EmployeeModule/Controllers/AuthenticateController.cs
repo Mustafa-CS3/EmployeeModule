c753461fa86b79306c9820c69ff62fa3ee50c934
Mustafa <muhammad.mustafa@celeritasdigital.com>
2022-05-25 17:34:07 +0500

    changes

--- EmployeeModule/Controllers/AuthenticateController.cs
+++ EmployeeModule/Controllers/AuthenticateController.cs
@@ -31,11 +31,8 @@ namespace EmployeeModule.Controllers
 
         [HttpPost]
         [Route("signup")]
-        public async Task<IActionResult> Register([FromBody] Signup model)
+        public async  Task<IActionResult> Register([FromBody] Signup model)
         {
-            var userExists = await userManager.FindByNameAsync(model.UserId);
-            if (userExists != null)
-                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists" });
 
             AppUser user = new AppUser()
             {
@@ -45,16 +42,16 @@ namespace EmployeeModule.Controllers
             };
             var result = await userManager.CreateAsync(user, model.Password);
             if (!result.Succeeded)
-                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Check your input" });
 
-            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+            return Ok(new Response { Status = "Success", Code = "200", Message = "User created successfully!" });
         }
 
         [HttpPost]
         [Route("login")]
-        public async Task<IActionResult> Login([FromBody] Signup model)
+        public async Task<IActionResult> Login([FromBody] Login model)
         {
-            var user = await userManager.FindByNameAsync(model.UserId);
+            var user = await userManager.FindByEmailAsync(model.UserId);
             if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
             {
                 var userRoles = await userManager.GetRolesAsync(user);
@@ -70,11 +67,10 @@ namespace EmployeeModule.Controllers
                     authClaims.Add(new Claim(ClaimTypes.Role, userRole));
                 }
 
-                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT: SecretKey"]));
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ijurkbdlhmklqacwqzdxmkkhvqowlyqa"));
 
                 var token = new JwtSecurityToken(
-                issuer: _configuration["JWT: ValidIssuer"],
-                audience: _configuration["JWT: ValidAudience"],
+            
                 expires: DateTime.Now.AddHours(3),
                 claims: authClaims,
                 signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
---
+++ EmployeeModule/Model/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeModule.Model
+{
+    public class Login
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "UserId is Required")]
+        public string UserId { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+        public string Password { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
